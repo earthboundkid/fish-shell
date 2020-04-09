b4ac2460c1e55d3e1402642973e7be3237ebab08
Carl Johnson <me@carlmjohnson.net>
2020-04-09 10:34:54 -0400

    Config: Set non-portable settings

--- config.fish
+++ config.fish
@@ -1,1 +1,6 @@
+# Setting here instead of fish_variables for portablity of $HOME
+test -e ~/bin && set PATH ~/bin $PATH
+test -e ~/src && set CDPATH ~/src $CDPATH .
+
+test -e ~/.misc && source ~/.misc
 test -e ~/.keys && source ~/.keys
